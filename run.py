0307dc2e590a31ce536f0baf3f5235f4c16ba0b5
AnJunYeong <whwnsdud4592@naver.com>
2024-12-22 19:59:52 +0900

    create (#1)

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ def tick():
             ball.rect.centerx = paddle.rect.centerx
             ball.rect.bottom = paddle.rect.top
 
-        ball.collide_block(BLOCKS)
+        ball.collide_block(BLOCKS, ITEMS)
         ball.collide_paddle(paddle)
         ball.hit_wall()
         if ball.alive() == False:
@@ -116,6 +116,11 @@ def main():
                 ball.draw(surface)
             for block in BLOCKS:
                 block.draw(surface)
+                
+            for item in ITEMS:
+                item.move()
+                item.draw(surface)
+                
 
         pygame.display.update()
         fps_clock.tick(config.fps)
